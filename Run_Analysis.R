33bfb29e628a80fee660a7c93444f577e54b5c9f
ZanaP <zana_pekmez@hotmail.com>
2018-10-08 00:05:18 +0200

    Create Run_Analysis.R

---
+++ Run_Analysis.R
@@ -0,0 +1,5 @@
+### Initialize target values:
+targetFolder <- 'UCI HAR Dataset'
+filename <- 'getdata_dataset.zip'
+### Get data from the internet:
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip',filename)
